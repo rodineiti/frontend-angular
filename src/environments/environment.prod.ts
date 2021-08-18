dfb2b69ed9a22b8998b4ec1ca4b265ca97e0f2da
Rodinei Developer <rodineiguitar@gmail.com>
2021-08-18 12:50:18 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  server_url: 'https://rdndeveloper.com/backend',
+  client_id: 2,
+  client_secret: "3hWqysh22NZbfYUFBw7SYoA6BH5akt4ta3z2Osk2"
 };
